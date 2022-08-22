f13fbda2bf0ced0cb5bb04e30dcc717ef64ecfe6
bhaskar199189 <110677511+bhaskar199189@users.noreply.github.com>
2022-08-22 11:52:50 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -18,4 +18,7 @@ def runSelenium(jobname)
   sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
 }
 
-
+def newMail(subject,body,email)
+{
+  mail bcc: '', body: "${body}", cc: '', from: '', replyTo: '', subject: "${subject}", to: "${email}"
+}
